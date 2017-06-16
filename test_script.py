f11070de0a3476733c5b465e3bd541d2dc984194
leaflux <longshorker@outlook.com>
2017-06-16 15:53:11 -0400

    Commit 4/16

--- test_script.py
+++ test_script.py
@@ -3,3 +3,11 @@ from script_1 import *
 class TestErrors:
     def test_inc(self):
         assert inc(1) == 2
+
+class TestMoreErrors:
+    def test_complex_error(self):
+        def f():
+            return 44
+        def g():
+            return 44
+        somefunc(f(), g())
